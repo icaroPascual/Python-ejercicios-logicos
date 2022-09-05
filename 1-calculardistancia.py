d0558af8445d75779de1cf7ef6452b8efbbf0cc1
icaroPascual <icaropascualik@gmail.com>
2022-09-04 22:45:20 -0300

    Create 1-calculardistancia.py

---
+++ 1-calculardistancia.py
@@ -0,0 +1,11 @@
+print("Calcular Distancia")
+
+print("Ingrese velocidad del movil")
+vel=float(input("velocidad: "))
+
+print("Ingrese tiempo")
+time=float(input("tiempo: "))
+
+dist=vel*time
+
+print("La distacia recorrida es: ",dist)
