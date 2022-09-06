b9dab839d9444e57cd39fd88c569910907a445df
icaroPascual <icaropascualik@gmail.com>
2022-09-05 23:35:32 -0300

    2-Calcular promedio

---
+++ 2-calcularpromedio.py
@@ -0,0 +1,12 @@
+print("Calcular promedio de notas")
+
+print("ingrese la nota del primer alumno")
+n1=int(input("n1: "))
+print("ingrese la nota del segundo alumno")
+n2=int(input("n2: "))
+print("ingrese la nota del tercer alumno")
+n3=int(input("n3: "))
+
+pro=int((n1+n2+n3)/3)
+
+print("El promedio de las notas es: ",pro)
