bb8d82546477edcfbecbae9b03a4ddef156fe566
icaroPascual <icaropascualik@gmail.com>
2022-09-15 20:44:00 -0300

    8-Promedio y superficie

---
+++ 8-promedioYsuperficie.py
@@ -0,0 +1,14 @@
+print("ejercicio 8: Calcular perimetro y superficie del rectangulo")
+
+
+print("ingrese base y altura")
+BASE=float(input("base: "))
+ALT=float(input("altura: "))
+
+
+SUP=BASE*ALT
+PER=2*(BASE+ALT)
+
+
+print("superficie: ",SUP)
+print("perimetro: ",PER)
