4b5f651e147110ea8e8a1d1ccacd45dc32c06d96
icaroPascual <icaropascualik@gmail.com>
2022-09-15 20:34:11 -0300

    7-Promedio de 2 numeros

---
+++ 7-promedio2numeros.py
@@ -0,0 +1,13 @@
+print("Ejercicio7: Promedio de dos numeros")
+
+
+print("ingrese notas: ")
+N1= int(input("N1: "))
+N2= int(input("N2: "))
+
+
+S=N1+N2
+P=S/2
+
+
+print("el promedio de las notas es: ",P)
